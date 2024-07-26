3fd179d543fd92792867175c776bf31744360b4a
peter chung <peter.chung.716@gmail.com>
2024-07-26 13:23:49 -0400

    Create user portal page and ticket submission form component

---
+++ app/(directory)/userportal/page.tsx
@@ -0,0 +1,11 @@
+import TicketSubmissionForm from '@/app/components/TicketSubmissionForm';
+
+export default function UserPortal() {
+  return (
+    <div>
+      <div>
+        <TicketSubmissionForm />
+      </div>
+    </div>
+  );
+}
---
+++ app/components/TicketSubmissionForm.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function TicketSubmissionForm() {
+  return (
+    <div>
+      <form>
+        <div>placeholder comp</div>
+      </form>
+    </div>
+  );
+}
