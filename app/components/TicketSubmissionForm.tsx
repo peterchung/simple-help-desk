a7be6cf833e7f53badc7ecc424803b60862aeb53
peter chung <peter.chung.716@gmail.com>
2024-07-26 13:33:58 -0400

    Render form inputs based on input type

--- app/components/TicketSubmissionForm.tsx
+++ app/components/TicketSubmissionForm.tsx
@@ -1,10 +1,76 @@
 'use client';
 
+const formInputs = [
+  {
+    inputType: 'input',
+    label: 'Name',
+    type: 'text',
+    placeholder: 'John Doe',
+    name: 'name',
+  },
+  {
+    inputType: 'input',
+    label: 'Email',
+    type: 'email',
+    placeholder: 'example@example.com',
+    name: 'email',
+  },
+  {
+    inputType: 'input',
+    label: 'Subject',
+    type: 'text',
+    placeholder: 'Subjet',
+    name: 'subject',
+  },
+  {
+    inputType: 'select',
+    label: 'Priority',
+    option: ['Select priority...', 'Low', 'Med', 'High'],
+    name: 'priority',
+  },
+  {
+    inputType: 'textarea',
+    label: 'Description of problem',
+    placeholder: 'Please describe your issue here...',
+    name: 'description',
+  },
+];
+
 export default function TicketSubmissionForm() {
   return (
     <div>
       <form>
-        <div>placeholder comp</div>
+        <div>
+          {formInputs.map((input, idx) => (
+            <div key={idx}>
+              <label>{input.label}</label>
+              {input.inputType === 'input' && (
+                <input
+                  id={input.label}
+                  type={input.type}
+                  name={input.name}
+                  placeholder={input.placeholder}
+                  required={true}
+                />
+              )}
+              {input.inputType === 'select' && input.option && (
+                <select id={input.label} name={input.name} required={true}>
+                  {input.option.map((priority, idx) => (
+                    <option key={idx}>{priority}</option>
+                  ))}
+                </select>
+              )}
+              {input.inputType === 'textarea' && (
+                <textarea
+                  id={input.label}
+                  name={input.name}
+                  placeholder={input.placeholder}
+                  required={true}
+                />
+              )}
+            </div>
+          ))}
+        </div>
       </form>
     </div>
   );
