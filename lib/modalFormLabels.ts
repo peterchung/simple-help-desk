9e3d847a0c0cdd5ea87da3db801e8d7f18f21493
peter chung <peter.chung.716@gmail.com>
2024-08-10 19:07:59 -0400

    Move formLabels object to modalFormLabels.ts

---
+++ lib/modalFormLabels.ts
@@ -0,0 +1,11 @@
+export const formLabels = {
+  requestedBy: 'Requested by:',
+  email: 'Email:',
+  dateCreated: 'Date Created:',
+  lastModified: 'Last Modified:',
+  subject: 'Subject:',
+  issueDescription: 'Issue description:',
+  status: 'Status:',
+  lastResponse: 'Last response:',
+  currentResponse: 'Response:',
+};
